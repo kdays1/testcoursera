3be6065093499c26a84da69cce7444ab51e390eb
unknown <caroodzg@gmail.com>
2022-03-11 14:05:14 -0600

    finalfix2

--- Angular_SAP/Module1/assigment/angular/app.js
+++ Angular_SAP/Module1/assigment/angular/app.js
@@ -6,27 +6,32 @@
 
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope){
-        $scope.lista = " ";
-        $scope.howmuch = "Hello";
+        $scope.lista = "";
+        $scope.howmuch = "Hello!";
+        $scope.yourlunch ="";
 
         $scope.checkIt = function (){
             var food = $scope.lista.split(',');
             var counter = 0;
-            for (var i=0; i<food.length; i++){
-                if(food[i]!=" "){
-                    counter = counter + 1;
+            if($scope.lista==""){
+                $scope.howmuch = "Please enter data first!";
+            }else{
+                for (var i=0; i<food.length; i++){
+                    if(food[i]!=" "){
+                        counter = counter + 1;
+                    }
+                }
+                console.log($scope.lista);
+                console.log(counter);
+                if (counter==0){
+                    $scope.howmuch = "Please enter data first!";
+                }else if (counter<=3) {
+                    $scope.howmuch = "Enjoy!";
+                } else {
+                    $scope.howmuch = "Too much, maybe?";
                 }
             }
-            console.log($scope.lista);
-            console.log(counter);
-            if (counter==0){
-                $scope.howmuch = "Please enter data first!";
-            }else if (counter<=3) {
-                $scope.howmuch = "Enjoy!";
-            } else {
-                $scope.howmuch = "Too much, maybe?";
-            }
-            $scope.lista = " ";
+            $scope.lista = "";
         };
     }
     })();
