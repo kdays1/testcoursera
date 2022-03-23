9f409bf19d83a7c5abec2befba730a68bf3e506d
unknown <caroodzg@gmail.com>
2022-03-23 12:45:29 -0600

    elseif

--- Angular_SAP/Module3/assigment/assets/js/app.js
+++ Angular_SAP/Module3/assigment/assets/js/app.js
@@ -35,12 +35,12 @@
             var promise = MenuSearchService.getMatchedMenuItems(searchItem);
 
             promise.then (function (founds){
-                if(founds.lenght>0){
+                if(founds.lenght!=0){
                     narrowit.result = founds;
                     console.log(narrowit.result);
                     narrowit.wasfound = true;
                     narrowit.wasnotfound = false;
-                } else {
+                } else if (founds.lenght===0) {
                     narrowit.result = '';
                     narrowit.wasnotfound = true;
                     narrowit.wasfound = false;
