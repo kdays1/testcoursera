4906ab178dc43310aec08eacf31ce061e346864a
unknown <caroodzg@gmail.com>
2022-03-29 09:44:54 -0600

    Page

--- Angular_SAP/Module4/assignment/js/menudata.service.js
+++ Angular_SAP/Module4/assignment/js/menudata.service.js
@@ -3,7 +3,6 @@
     
     angular.module('data')
     .service('MenuDataService',MenuDataService)
-    .constant('Page',"https://davids-restaurant.herokuapp.com");
 
     MenuDataService.$inject = ['$http'];
     function MenuDataService($http){
@@ -11,14 +10,14 @@
         service.getAllCatergories = function(){
             var categ = $http({
                 method: "GET",
-                url: (Page + "/categories.json")
+                url: ("https://davids-restaurant.herokuapp.com/categories.json")
             });
                 return categ;
         };
         service.getItemsForCategory = function(categoryShortName){
             var catsn = $http({
                 method: "GET",
-                url: (Page + "/menu_items.json"),
+                url: ("https://davids-restaurant.herokuapp.com/menu_items.json?category="),
                 params: {
                     short_name : categoryShortName
                 }
