97b0157be939f243627f435000ef8e055f1a0eac
unknown <caroodzg@gmail.com>
2022-04-01 13:21:47 -0600

    ;inmodulesinginfo

--- Angular_SAP/Module5/Lecture59/src/public/signinfo/signinfo.module.js
+++ Angular_SAP/Module5/Lecture59/src/public/signinfo/signinfo.module.js
@@ -2,6 +2,6 @@
     "use strict";
     
     angular.module('signinfo', [])
-    .constant('ThePath', 'https://banana-custard-97785.herokuapp.com')
+    .constant('ThePath', 'https://banana-custard-97785.herokuapp.com');
     })();
     
