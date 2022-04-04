e7c6ff7cd58e3afcf610dba0a664fb9d57be5920
unknown <caroodzg@gmail.com>
2022-04-04 10:30:21 -0500

    referencesonHTML

--- Angular_SAP/Module5/Lecture59/src/public/signinfo/signup/signup.cotroller.js
+++ Angular_SAP/Module5/Lecture59/src/public/signinfo/signup/signup.cotroller.js
@@ -11,12 +11,18 @@
         ctrl.user = {};
         ctrl.errorocurred = false;
         ctrl.initialize = true;
+        ctrl.usuario.name = ctrl.user.fname;
+        ctrl.usuario.lname = ctrl.user.lname;
+        ctrl.usuario.email = ctrl.user.email;
+        ctrl.usuario.phone = ctrl.user.phone;
+        ctrl.usuario.menu = ctrl.user.menu;
         //var cat = $scope.user.menu;
         console.log(ctrl.user);
         ctrl.submit = function(){
             var promise = SignInfoService.signUpMenu(ctrl.user.menu);
             promise.then(function(signupcategory){
                     ctrl.user.result = signupcategory.data;
+                    ctrl.usuario.result = ctrl.user.result;
                     console.log(ctrl.user.result);
                     ctrl.errorocurred = false;
                     ctrl.initialize = false;
